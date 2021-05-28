a54e564a5432d827a9ae1dcf9819ae26f91205ca
Rakesh DR <rakeshdr543@gmail.com>
2021-05-28 10:41:40 +0530

    Added fetch products ass array

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -1,6 +1,15 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+var_dump($products);
+echo '</pre>';
+
+
 ?>
 
 <!doctype html>
